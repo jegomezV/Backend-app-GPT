52a3384d94e17d930c72f04d06f0db4dd12d162c
jegomezV <jegomezvalencia@gmail.com>
2025-01-22 19:55:42 -0500

    Finish fixes

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,12 +2,10 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from '../modules/users/entity/users.entity';
 import { Chat } from '../modules/chat/entity/chat.entity';
 import { Message } from '../modules/chat/entity/message.entity';
-import * as dotenv from 'dotenv';
 
-dotenv.config();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
+  type: 'mysql',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10),
   username: process.env.DB_USERNAME,
